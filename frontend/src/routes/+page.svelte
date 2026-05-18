038215b96c700f504343a51cb64a2b6b87605f4c
Jordan <jyodann@outlook.com>
2026-05-18 17:33:30 +0800

    Added some frontend code

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,30 +1,60 @@
 <script lang="ts">
-    const currentDay = new Date().getDay();
-    // Get Localised Sunday to Sat:
-    const daysOfWeek = [
-        new Date("25 Jan 2026"), // Sunday
-        new Date("26 Jan 2026"),
-        new Date("27 Jan 2026"),
-        new Date("28 Jan 2026"),
-        new Date("29 Jan 2026"),
-        new Date("30 Jan 2026"),
-        new Date("31 Jan 2026"), // Monday
-    ]
+	import { SvelteDate } from "svelte/reactivity";
 
-    const dateFormatter = new Intl.DateTimeFormat(undefined, {weekday: "short"})
-    
-    function formatShortDate(date: Date): string {
-        return dateFormatter.format(date);
+	const currentDay = new Date().getDay();
+	// Get Localised Sunday to Sat:
+	const daysOfWeek = [
+		new Date('25 Jan 2026'), // Sunday
+		new Date('26 Jan 2026'),
+		new Date('27 Jan 2026'),
+		new Date('28 Jan 2026'),
+		new Date('29 Jan 2026'),
+		new Date('30 Jan 2026'),
+		new Date('31 Jan 2026') // Monday
+	];
+
+
+	const dateFormatter = new Intl.DateTimeFormat(undefined, { weekday: 'short' });
+
+    const timeFormatter = new Intl.DateTimeFormat(undefined, {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+
+
+	function formatShortDate(date: Date): string {
+		return dateFormatter.format(date);
+	}
+
+    const dateTimeToSet = new SvelteDate('25 Jan 2026');
+
+    function formatShortTime(hour: number): string {
+        return timeFormatter.format(dateTimeToSet.setHours(hour, 0, 0, 0))
     }
-
-    
 </script>
 
-<div>
-
-    <div class="columns-5 text-center">
-        {#each {length: 5}, i}
-            <div>{formatShortDate(daysOfWeek[i+1])} </div>
-        {/each}
-    </div>
+<div class="flex">
+	<div class="row-auto w-12 flex-initial">
+        <div class="h-16"></div>
+		{#each { length: 13 }, i}
+			<div class="h-16 text-center align-middle">
+                <p class="relative bottom-3">{formatShortTime(i + 8)}</p>
+            </div>
+		{/each}
+	</div>
+	<div class="flex-1">
+		<div class="columns-5 text-center gap-2">
+			{#each { length: 5 }, i}
+				<div class="h-16 border-2">{formatShortDate(daysOfWeek[i + 1])}</div>
+				{#each { length: 12 }, i}
+                    {#if i % 2 == 0}
+					    <div class="h-16 text-center bg-blue-300">{i}</div>
+                    {:else}
+					    <div class="h-16 text-center bg-blue-400">{i}</div>
+                    {/if}
+                    
+				{/each}
+			{/each}
+		</div>
+	</div>
 </div>
