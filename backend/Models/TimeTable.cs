cf01886ca28488f62ff078b75c327b8f7f902929
ZZz <zhuzhen.zhuo@gmail.com>
2026-05-28 00:26:34 +0800

    feat: added required to fields for better type checking

--- backend/Models/TimeTable.cs
+++ backend/Models/TimeTable.cs
@@ -5,24 +5,24 @@ namespace Backend.Models;
 
 public class TimeTableModule
 {
-    public string ModuleCode { get; set; } = string.Empty;
-    public string LessonNo { get; set; } = string.Empty;
-    public string LessonType { get; set; } = string.Empty;
+    public required string ModuleCode { get; set; } = string.Empty;
+    public required string LessonNo { get; set; } = string.Empty;
+    public required string LessonType { get; set; } = string.Empty;
 }
 
 public class TimeTable
 {
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
 
     // No FK constraint since users is handled by supabase auth, and apparently this is standard practice for supabase auth since they have thier own table
     [JsonIgnore]
-    public Guid UserId { get; set; }
+    public required Guid UserId { get; set; }
     public string Name { get; set; } = string.Empty; // Name of the time table, selected by user
 
     // The following fields are placed here, instead of TimeTableModule since all the modules will havae the same semester and academic year
-    public int Semester { get; set; }
-    public string AcademicYear { get; set; } = string.Empty;
+    public required int Semester { get; set; }
+    public required string AcademicYear { get; set; } = string.Empty;
 
     [Column(TypeName = "jsonb")]
-    public List<TimeTableModule> MetaData { get; set; } = new();
+    public required List<TimeTableModule> MetaData { get; set; }
 }
