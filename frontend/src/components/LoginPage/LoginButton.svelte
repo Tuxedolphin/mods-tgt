48ddaaaa6a282160f223855cbb034da9086fc3f9
Jordan <jyodann@outlook.com>
2026-05-29 11:09:59 +0800

    Refactor to Components

---
+++ frontend/src/components/LoginPage/LoginButton.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import { onMount } from 'svelte';
+	import { currentUserInformation } from '../../shared/shared.svelte';
+	import { login_to_db } from '../../utils/db_operations';
+
+	interface LoginButtonProps {
+		email: string;
+		password: string;
+	}
+	let loading = $state(false);
+	let { email, password }: LoginButtonProps = $props();
+	let errorMessage = $state('');
+	onMount(() => {
+		if ($currentUserInformation.displayName) {
+			goto(resolve('/planner'));
+		}
+	});
+
+	async function login() {
+		loading = true;
+		const result = await login_to_db(email, password);
+		if (result.isOk()) {
+			// Stores access token in localstorage (FOR NOW) -- Not secure:!
+			localStorage.setItem('ac:tok', result.value.access_token);
+			goto(resolve('/planner'));
+		} else {
+			errorMessage = result.error;
+		}
+		loading = false;
+	}
+</script>
+
+{#if loading}
+	<button class="disabled btn mt-4 btn-neutral" type="submit">Logging in...</button>
+{:else}
+	<button class="btn mt-4 btn-neutral" type="submit" onclick={() => login()}>Login</button>
+{/if}
+
+{#if errorMessage}
+	{errorMessage}
+{/if}
---
+++ frontend/src/components/LoginPage/LoginComponent.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import LoginButton from './LoginButton.svelte';
+
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import { onMount } from 'svelte';
+	import { currentUserInformation } from '../../shared/shared.svelte';
+
+	let emailInput = $state('');
+	let passwordInput = $state('');
+	onMount(() => {
+		if ($currentUserInformation.displayName) {
+			goto(resolve('/planner'));
+		}
+	});
+</script>
+
+<div>Login Here!</div>
+<form class="fieldset content-around px-4">
+	<fieldset class="fieldset">
+		<label class="label">Email</label>
+		<input
+			type="email"
+			class="validator input w-full"
+			placeholder="Email"
+			required
+			bind:value={emailInput}
+		/>
+		<p class="validator-hint hidden">Required</p>
+	</fieldset>
+
+	<label class="fieldset">
+		<span class="label">Password</span>
+		<input
+			type="password"
+			class="validator input w-full"
+			placeholder="Password"
+			required
+			bind:value={passwordInput}
+		/>
+		<span class="validator-hint hidden">Required</span>
+	</label>
+
+	<LoginButton email={emailInput} password={passwordInput}></LoginButton>
+</form>
+<div>
+	Don't have an account yet? Register <button
+		class="cursor-pointer underline"
+		onclick={() => goto(resolve('/register'))}>here</button
+	>
+</div>
