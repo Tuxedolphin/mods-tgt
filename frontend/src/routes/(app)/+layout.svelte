66d54569d7b5ed2fcca7481db828a429ede14a43
Jordan <jyodann@outlook.com>
2026-06-11 14:26:22 +0800

    Added Sidebar to Home menu

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,19 +1,38 @@
 <script lang="ts">
 	import type { LayoutProps } from './$types';
 	import mods_tgt_header from '$lib/assets/mods_tgt_header.png?enhanced';
+	import { Menu } from '@lucide/svelte';
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	let { data, children }: LayoutProps = $props();
 </script>
 
-<div class="navbar bg-base-200 shadow-sm">
-	<!-- svelte-ignore a11y_missing_attribute -->
-	<enhanced:img
-		class="aspect-5/2 h-14 w-auto align-middle"
-		src={mods_tgt_header}
-		alt="Mods Together Logo"
-	/>
-</div>
+<div class="drawer">
+	<input id="main-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<div class="navbar bg-base-200 shadow-sm">
+			<div class="flex items-center">
+				<label for="main-drawer" aria-label="open sidebar">
+					<Menu></Menu>
+				</label>
+				<enhanced:img
+					class="aspect-5/2 h-14 w-auto align-middle"
+					src={mods_tgt_header}
+					alt="Mods Together Logo"
+				/>
+			</div>
+		</div>
 
-<div class="px-2 pt-2">
-	{@render children()}
+		<div class="px-2 pt-2">
+			{@render children()}
+		</div>
+	</div>
+
+	<div class="drawer-side">
+		<label for="main-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu min-h-full w-64 bg-base-200 p-4">
+			<!-- Sidebar content here -->
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
 </div>
